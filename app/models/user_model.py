da3278bf05c37ba183ae12396555ad9e76e2c92a
umutbayramoglu <umut265@gmail.com>
2020-11-08 02:39:26 +0300

    Create user model.

---
+++ app/models/user_model.py
@@ -0,0 +1,12 @@
+from app.models.base_model import BaseModel
+
+
+class UserModel(BaseModel):
+    """ Model class for the user objects """
+
+    def __init__(self, email, password, name, surname):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.surname = surname
+        super().__init__(id=0, create_date="created_date", update_date="updated_date")
