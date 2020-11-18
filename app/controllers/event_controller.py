eef1f0de7800c69ac2f0a9f1d5bd6cc2731b6fb9
umutbayramoglu <umut265@gmail.com>
2020-11-18 18:18:06 +0300

    Implement token decorator.

--- app/controllers/event_controller.py
+++ app/controllers/event_controller.py
@@ -1,11 +1,14 @@
 from app.repository.event_repository import EventRepository
 from flask import Blueprint, request
 
+from app.services.auth_service import require_token
+
 event_api = Blueprint('event_api', __name__)
 er = EventRepository()
 
 
 @event_api.route("/events", methods=['POST'])
+@require_token(request)
 def create_event():
     """
     Handle api requests to create new event.
@@ -21,7 +24,6 @@ def create_event():
     }
     :return: HTTP Status
     """
-
     er.create_event(request.get_json())
     return "ok"
 
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,4 +1,6 @@
-from flask import current_app
+from functools import wraps
+
+from flask import current_app, make_response
 
 from app.repository.user_repository import UserRepository
 import jwt
@@ -7,7 +9,47 @@ import datetime
 user_repo = UserRepository()
 
 
+def require_token(request):
+    """
+    Decorator function to check Jwt token is valid or not.
+    This decorator must be used with functions that requires login permission.
+    :param request: Http request
+    """
+
+    def inside_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            headers = request.headers
+
+            if "Authorization" not in headers:
+                return make_response("Token is not correct.", 403,
+                                     {'WWW-Authenticate': 'Basic realm="Token is not correct."'})
+
+            token = headers['Authorization'].replace("Bearer ", "")
+            if not token:
+                return make_response("Token is not correct.", 403,
+                                     {'WWW-Authenticate': 'Basic realm="Token is not correct."'})
+            try:
+                data = jwt.decode(token, current_app.config['SECRET_KEY'])
+                print(data)
+            except Exception as e:
+                return make_response(str(e), 403, {'WWW-Authenticate': 'Basic realm="Token is not correct."'})
+
+            res = func(*args, **kwargs)  # If token is valid, execute the real function
+            return res
+
+        return wrapper
+
+    return inside_decorator
+
+
 def login(email, password):
+    """
+    If the credentials are correct, it generates the JWT token.
+    :param email
+    :param password
+    :return: A dictionary that contains the token and authenticated user data.
+    """
     user = user_repo.authenticate(email, password)
     claims = {'username': email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(weeks=2)}
     token = jwt.encode(
