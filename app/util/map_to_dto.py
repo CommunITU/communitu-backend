0fc929c0af4a509ad3d28ad0bc58ced1dd3b5e81
umutbayramoglu <umut265@gmail.com>
2020-11-19 20:43:15 +0300

    Add pydoc to map_to_dto.py.

--- app/util/map_to_dto.py
+++ app/util/map_to_dto.py
@@ -1,7 +1,15 @@
+# DATA TRANSFER OBJECT MODELS
 event_model_dto = ['id', 'title', 'description', 'start_date', 'end_date']
 user_model_dto = ['id', 'email', 'name', 'surname']
 
 
 def map_to_dto(dao, dto_model):
+    """
+    Converts data access object to data transfer object.
+
+    :param dao: Data access object to be converted to its dto model.
+    :param dto_model: A list that specifies the dto object fields.
+    :return: Dto
+    """
     dto = {field: dao[field] for field in dto_model}
     return dto
