6e43937d5c0aae365c1e91f2cbbc4833874c1367
umutbayramoglu <umut265@gmail.com>
2020-11-29 02:28:38 +0300

    Create club controller.

---
+++ app/controllers/club_controller.py
@@ -0,0 +1,16 @@
+from app.repository.club_repository import ClubRepository
+from flask import Blueprint, request, jsonify, make_response
+
+from app.services.auth_service import require_token
+
+club_api = Blueprint('club_api', __name__)
+er = ClubRepository()
+
+
+@club_api.route("/clubs", methods=['POST'])
+@require_token(request)
+def create_club(user_id="", user_email=""):
+    # TODO: ADD PYDOC
+
+    # TODO: IMPLEMENT SERVICE AND REPOSITORY FUNCTIONS
+    return "ok"
--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from flask_cors import CORS
 
 from app.constants.app_constants import TOKEN_SECRET_KEY
 from app.controllers.auth_controller import auth_api
+from app.controllers.club_controller import club_api
 from app.repository.authority_repository import AuthorityRepository
 from app.repository.club_repository import ClubRepository
 from app.repository.comment_repository import CommentRepository
 from app.repository.event_repository import EventRepository
+from app.repository.notification_repository import NotificationRepository
 from app.repository.user_repository import UserRepository
 from app.controllers.event_controller import event_api
 
@@ -14,6 +16,7 @@ from app.controllers.event_controller import event_api
 def register_controllers(m_app):
     m_app.register_blueprint(event_api)  # register event controller
     m_app.register_blueprint(auth_api)  # register auth controller
+    m_app.register_blueprint(club_api)  # register club controller
 
 
 def init_db():
@@ -21,6 +24,7 @@ def init_db():
     AuthorityRepository().initialize_table()  # init authority table
     EventRepository().initialize_table()  # init event table
     ClubRepository().initialize_table()  # init club table
+    NotificationRepository().initialize_table()  # init notification table
     CommentRepository().initialize_table()  # init comment table
 
 
