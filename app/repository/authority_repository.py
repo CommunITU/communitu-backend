0b7b8dd0ecfa0d290603c0d1b137bf5f9f251e4b
umutbayramoglu <umut265@gmail.com>
2020-12-15 20:20:11 +0300

    Add authority methods

--- app/repository/authority_repository.py
+++ app/repository/authority_repository.py
@@ -1,5 +1,6 @@
 from app.constants.database_constants import AUTHORITY_TABLE_NAME, AUTHORITY_TABLE_INIT_STAT, \
-    LINKER_USER_AUTHORITY_TABLE_INIT_STAT
+    LINKER_USER_AUTHORITY_TABLE_INIT_STAT, \
+    LINKER_USER_AUTHORITY_TABLE_NAME
 from app.repository import BaseRepository
 
 
@@ -20,5 +21,18 @@ class AuthorityRepository(BaseRepository):
         super().initialize_table(initialization_statement=LINKER_USER_AUTHORITY_TABLE_INIT_STAT)
 
     @classmethod
-    def create_authority(cls, authority):
-        super().add(authority, table_name=AUTHORITY_TABLE_NAME)
+    def create_authority(cls, authority_data):
+        super().add(data=authority_data, table_name=AUTHORITY_TABLE_NAME)
+
+    @classmethod
+    def get_authority_id_by_name(cls, authority_name):
+        id = super().select(from_tables=[AUTHORITY_TABLE_NAME], return_columns=["id"],
+                            where={"authority": authority_name})[0]["id"]
+        return id
+
+    @classmethod
+    def set_authority_of_user(cls, authority, user_id):
+        authority_id = cls.get_authority_id_by_name(authority)
+
+        map_user_auth = {"user_id": user_id, "authority_id": authority_id}
+        super().add(map_user_auth, table_name=LINKER_USER_AUTHORITY_TABLE_NAME)
